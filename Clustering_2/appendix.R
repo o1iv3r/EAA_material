369b27fd936a46a46b86642fe365627849c70ac1
Oliver Pfaffel <opfaffel@gmail.com>
2021-06-19 13:19:22 +0200

    some cleanup

--- Clustering_2/appendix.R
+++ Clustering_2/appendix.R
@@ -43,3 +43,64 @@ plot(mclust_res,what="classification",addEllipses =TRUE)
 
 
 
+
+### Some mixed data type stuff
+
+Some algorithms cannot deal with categorical data. However, one may compute the distance matrix a-priori using Gower's distance and use this as an input to the clustering algorithm. With that, each variable (column) is first standardized by dividing each entry by the range of the corresponding variable, after subtracting the minimum value; consequently the rescaled variable has range [0,1], exactly.
+
+```{r}
+library(cluster)
+dist_dat_no_missings<- daisy(dat_no_missings,
+                                     metric = "gower",
+                                     stand = TRUE)
+```
+
+```{r}
+dat_no_missings_onehot <- model.matrix(paste("~0 +",
+                                             paste(colnames(dat_no_missings),collapse=' + ')),
+                                       data = dat_no_missings)
+```
+```{r}
+library(mltools)
+dat_no_missings_onehot_scaled <- scale(one_hot(dat_no_missings))
+```
+
+
+
+```{r}
+dat_no_missings_numeric = dat_no_missings[,mget(c(num_vars))]
+```
+
+
+FOR NOW USE THESE DATA MATRICES
+
+```{r}
+dat_num_no_missings_scaled <- scale(dat_no_missings_numeric)
+```
+
+
+
+```{r}
+pam_res <- pam(dist_dat_no_missings, k= nr_clusters)
+```
+
+The medoids are quite hard to interpret since they are referring to the distance matrix, but we will only look at the cluster assignments and compare this with result from above when only numeric variables are used
+
+```{r}
+library(ClusterR)
+external_validation(true_labels = pam_res$clustering, clusters = predict(cclust_res))
+```
+
+
+```{r}
+pam_numeric_res <- pam(dat_num_no_missings_scaled, k=nr_clusters)
+external_validation(true_labels = pam_numeric_res$clustering, 
+                    clusters = predict(cclust_res))
+```
+
+
+```{r}
+cclust_onehot_res <- cclust(dat_no_missings_onehot_scaled , k=nr_clusters)
+```
+
+
